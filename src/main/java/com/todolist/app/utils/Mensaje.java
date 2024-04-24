b42dfb4eabc2914168eddc052a2af9dfd790c3c4
Santiago Quintero <86493587+santiqc@users.noreply.github.com>
2024-04-23 21:33:52 -0500

    Creacion de clase de mensajes

---
+++ src/main/java/com/todolist/app/utils/Mensaje.java
@@ -0,0 +1,15 @@
+package com.todolist.app.utils;
+
+public class Mensaje {
+
+    public static final String TASK_NO_ENCONTRADA = "No se encontro la tarea para ser eliminada!";
+    public static final String TASK_ELIMINADA = "Tarea eliminada correctamente.";
+    public static final String TASK_GUARDADA = "Tarea guardada correctamente.";
+
+    public static final String TASK_ACTUALIZADA = "Tarea actualizada correctamente.";
+
+    public static final String IDENTIFICADOR_NULL = "El identificador de la task no puede ser nulo!";
+
+    public static final String TASK_NO_ENCONTRADA_ACTUALIZARLA = "No se encontro la tarea para ser actualizada!";
+
+}
